0b4ae7795df4d3ff96e34b75cb2f638d17388114
efrainjato <efrainjacobotovar@gmail.com>
2018-03-06 15:44:35 -0600

    navbar color

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,40 +4,6 @@
 @section('content')
 
 <h1>Home</h1>
-<div class="container">
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-        <div class="item active">
-            <img src="img/imagen1.jpg" alt="">
-        </div>
-        <div class="item">
-            <img src="img/imagen2.jpg" alt="">
-        </div>
-        <div class="item">
-            <img src="img/imagen3.jpg" alt="">
-        </div>
-    </div>
-
-    <!-- Controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
-</div>
-<br>
 
 <div class="container" style="font-size: 2em; font-weight: 700; color: black">
 <p>
