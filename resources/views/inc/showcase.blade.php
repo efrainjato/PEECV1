80ff6f9446e039924b62c4bdf70257c6958ee4ae
efrainjato <efrainjacobotovar@gmail.com>
2018-03-06 12:36:40 -0600

    jumbotron

--- resources/views/inc/showcase.blade.php
+++ resources/views/inc/showcase.blade.php
@@ -1,6 +1,6 @@
-<div class="container">
+<div class="container" style="background-color: black;">
 	<div class="row">
-		<div class="col-md-12 jumbotron">
+		<div class="col-md-12 jumbotron" style="background-color: white;">
 			<div class="text-center">
 					<h1 style="color: black; font-weight: 700;">Programa de Evaluación Externa de la Calidad</h1>
 	                <p class="lead">¡Bienvenido al portal!<p>
